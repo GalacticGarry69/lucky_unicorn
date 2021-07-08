12bc334f671dc7cbba03bccc337aad0970619edb
GalacticGarry69 <strach.castr@awatapu.school.nz>
2021-07-09 09:44:09 +1200

    Gets a number and *5

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,1 +1,10 @@
 print ("Hello world")
+
+# Ask user for number
+get_number = int(input("Choose a number? "))
+
+# Multiply the number by 5
+times_five = get_number * 5
+
+# Output the result
+print(times_five)
